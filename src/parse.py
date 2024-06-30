660b4096974af8905dc98dcb42cd329e564e3b6c
maxim <ygstoresup@gmail.com>
2024-06-30 23:32:07 +0300

    Improved code somewhat

--- src/parse.py
+++ src/parse.py
@@ -6,6 +6,7 @@ class ParserInstance:
         self.get_vacancies_timeout = config['get_vacancies_timeout']
         self.get_resume_timeout = config['get_resume_timeout']
         self.resume_links_timeout = config['resume_links_timeout']
+        
         self.schedule_dict = [{"id":"fullDay","name":"Полный день","uid":"full_day"},
                               {"id":"shift","name":"Сменный график","uid":"shift"},
                               {"id":"flexible","name":"Гибкий график","uid":"flexible"},
@@ -41,24 +42,15 @@ class ParserInstance:
 
         try:
             r = httpx.get('https://api.hh.ru/vacancies' + params[:-1], timeout=self.get_vacancies_timeout)
-            return r.json()['items']
-        except Exception as exc:
+        except:
             return None
+        
+        result = []
 
-    def get_resume_links(self, page=0, query_text=''):
-        try:
-            r = httpx.get(f'https://hh.ru/search/resume' + query_text, follow_redirects=True, params={'page': page})
-        except httpx.TimeoutException as exc:
-            return None
-        soup = BeautifulSoup(r.text, 'html.parser')
-        soup = soup.find(attrs={'data-qa': 'resume-serp__results-search'})
-            
-        if soup:
-            links = []
-            for el in soup.find_all('a', attrs={'class': 'bloko-link'}):
-                links.append('https://hh.ru' + el['href'])
-            return links
-        return None
+        for item in r.json()['items']:
+            result.append(self.__get_vacancy_params(item))
+
+        return result
 
     def get_resumes(self, page=0, text=None, experience=None, schedule=None, salary=None, employment=None):
         params = f'?page={page}&per_page=20&'
@@ -75,46 +67,46 @@ class ParserInstance:
                 params += f'schedule={param}&'
         if salary:
             params += f'salary_from={int(salary - 0.1*salary)}&salary_to={int(salary + 0.1*salary)}&label=only_with_salary&'
-        links = self.get_resume_links(query_text=params[:-1])
+        links = self.__get_resume_links(query_text=params[:-1])
         
         result = []
 
         for link in links:
-            result.append(self.get_resume_params(link))
+            try:
+                r = httpx.get(link, follow_redirects=True, timeout=self.get_resume_timeout)
+                soup = BeautifulSoup(r.text, 'html.parser')
+            except:
+                continue
+
+            params = self.__get_resume_params(soup)
+            params['id'] = link.split('?')[0].split('/')[-1]
+            result.append(params)
         
         return result
 
-    # reformats \xa0 spaces 
-    def fix_spaces(text):
-        return ' '.join(text.split())
-
-    def get_text_qa(soup, name):
-        res = soup.find(attrs={'data-qa': name})
-        if res:
-            return ParserInstance.fix_spaces(res.text)
-        return None
-
-    def get_vacancy_params(self, item):
-
+    def __get_vacancy_params(self, item):
         params = {
             'id': item['id'],
             'name': item['name'],
             'area': item['area']['name'],
-            'average_salary': None,
-            'currency': None if not item['salary'] else item['salary']['currency'],
             'type': item['type']['name'],
             'employer': item['employer']['name'],
-            'requirement': None,
             'responsibility': item['snippet']['responsibility'],
             'schedule': item['schedule']['name'],
             'experience': item['experience']['name'],
-            'employment': item['employment']['name']
+            'employment': item['employment']['name'],
+
+            'average_salary': None,
+            'requirement': None,
+            'currency': None 
         }
 
         if item['snippet']['requirement']:
             params['requirement'] = item['snippet']['requirement'].replace('<highlighttext>', '').replace('</highlighttext>', '')
 
         if item['salary']:
+            params['currency'] = item['salary']['currency']
+
             if item['salary']['from']:
                 if item['salary']['to']:
                     params['average_salary'] = (int(item['salary']['from']) + int(item['salary']['to'])) // 2
@@ -122,20 +114,22 @@ class ParserInstance:
                     params['average_salary'] = int(item['salary']['from'])
             else:
                 params['average_salary'] = int(item['salary']['to'])
+
         return params
 
-    def get_resume_params(self, resume_link):
+    def __get_resume_params(self, soup):
         params = {
-            'id': None,
-            'gender': None,
-            'age': None,
-            'birthday': None,
-            'search_status': None,
-            'address': None,
-            'position': None,
+            'gender': ParserInstance.text_by_qa(soup, 'resume-personal-gender'),
+            'age': ParserInstance.text_by_qa(soup, 'resume-personal-age'),
+            'birthday': ParserInstance.text_by_qa(soup, 'resume-personal-birthday'),
+            'search_status': ParserInstance.text_by_qa(soup, 'job-search-status'),
+            'address': ParserInstance.text_by_qa(soup, 'resume-personal-address'),
+            'position': ParserInstance.text_by_qa(soup, 'resume-block-title-position'),
+            'about': ParserInstance.text_by_qa(soup, 'resume-block-skills-content'),
+
             'specializations': None,
-            'about': None,
             'salary': None,
+            'currency': None,
             'preferred_commute_time': None,
             'skills': None,
             'employment': None,
@@ -149,28 +143,11 @@ class ParserInstance:
             'schedule': None,
         }
 
-        try:
-            r = httpx.get(resume_link, follow_redirects=True, timeout=self.get_resume_timeout)
-        except httpx.TimeoutException as exc:
-            return params
-        
-        soup = BeautifulSoup(r.text, 'html.parser')
-        if not soup: return None
-
-        params['id'] = resume_link.split('?')[0].split('/')[-1]
-        params['gender'] = ParserInstance.get_text_qa(soup, 'resume-personal-gender')
-        params['age'] = ParserInstance.get_text_qa(soup, 'resume-personal-age')
-        params['birthday'] = ParserInstance.get_text_qa(soup, 'resume-personal-birthday')
-        params['search_status'] = ParserInstance.get_text_qa(soup, 'job-search-status')
-        params['address'] = ParserInstance.get_text_qa(soup, 'resume-personal-address')
-        params['position'] = ParserInstance.get_text_qa(soup, 'resume-block-title-position')
-        params['about'] = ParserInstance.get_text_qa(soup, 'resume-block-skills-content')
-
-        data = ParserInstance.get_text_qa(soup, 'resume-block-position-specialization')
+        data = ParserInstance.text_by_qa(soup, 'resume-block-position-specialization')
         if data:
             params['specializations'] = data.replace(', ', ',').split(',')
 
-        data = ParserInstance.get_text_qa(soup, 'resume-block-salary')
+        data = ParserInstance.text_by_qa(soup, 'resume-block-salary')
         if data:
             params['salary'] = ''.join(data.split(' ')[:-2])
 
@@ -211,3 +188,26 @@ class ParserInstance:
             params['work_prev_pos'] = list(map(lambda x: ParserInstance.fix_spaces(x.text), data))
 
         return params
+
+    def __get_resume_links(self, query_text=''):
+        try:
+            r = httpx.get(f'https://hh.ru/search/resume' + query_text, follow_redirects=True, timeout=self.resume_links_timeout)
+            soup = BeautifulSoup(r.text, 'html.parser')
+        except:
+            return None
+
+        soup = soup.find(attrs={'data-qa': 'resume-serp__results-search'}) # Limits parse area
+            
+        if soup:
+            soup = ['https://hh.ru' + el['href'] for el in soup.find_all('a', attrs={'class': 'bloko-link'})] # Actual links
+        return soup
+
+    def text_by_qa(soup, name):
+        res = soup.find(attrs={'data-qa': name})
+        if res:
+            return ParserInstance.fix_spaces(res.text)
+        
+        return None
+    
+    def fix_spaces(text): # reformats \xa0 spaces
+        return ' '.join(text.split())
